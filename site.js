6a984badf3e564ad821f1a368eadb74353ed3ee3
kFadi <kashan.fadi@gmail.com>
2022-06-27 13:54:35 +0300

    version 3

--- site.js
+++ site.js
@@ -1,3 +1,4 @@
+const app_ver = "App Version 3.0";
 const max_addons = 3;
 const initial_price = 42;
 
@@ -85,7 +86,7 @@ function btnResetFcn() {
   checkKosherFcn();
   updateBurgerFcn();
   checkHealthyFcn();
-  checkFriesFcn();
+  checkFriesSizeFcn();
   updateDrinkFcn();
 
   document.getElementById("btn-reset").disabled = true;
@@ -108,6 +109,7 @@ function update_price() {
 function checkKosherFcn() {
   const isKosher = document.getElementById("check-kosher").checked;
   const logo_img = document.getElementById("logo-img");
+  logo_img.title = app_ver;
 
   if (isKosher) {
     while (cnt1 > 0) {
@@ -135,10 +137,10 @@ function checkKosherFcn() {
 
 function createBurger(idx) {
   const img = document.createElement("img");
-  if (idx == 0) {
+  if (idx === 0) {
     img.src = "/assets/meal/burger_beef.png";
     img.alt = "BEEF BURGER";
-  } else if (idx == 1) {
+  } else if (idx === 1) {
     img.src = "/assets/meal/burger_chicken.png";
     img.alt = "CHICKEN BURGER";
   } else {
@@ -204,122 +206,118 @@ function checkHealthyFcn() {
   document.getElementById("btn-reset").disabled = false;
 }
 
-function checkFriesFcn() {
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+function checkFriesSizeFcn() {
   const checked = document.getElementById("check-fries").checked;
   total_price += checked ? price_size_fries : -1 * price_size_fries;
   update_price();
 
-  const controller = document.getElementById("next-to-fries");
-  controller.innerHTML = "";
   const img = document.createElement("img");
+  img.id = "fries-img";
+  img.className = "size-" + (checked ? "plus" : "reg");
 
   if (checked) {
     img.src = "/assets/fries/fries_size.png";
     img.alt = "FRIES SIZE";
-    img.id = "fries-im-size";
   } else {
     img.src = "/assets/fries/fries.png";
     img.alt = "FRIES";
-    img.id = "fries-im";
   }
 
+  const controller = document.getElementById("next-to-fries");
+  controller.innerHTML = "";
   controller.appendChild(img);
 
   document.getElementById("btn-reset").disabled = false;
 }
 
 //////////////////////////////////////////////
-//////////////////////////////////////////////
 
-function updateDrinkFcn() {
-  const isZero = document.getElementById("check-zero").checked;
-  const isSize = document.getElementById("check-drink-size").checked;
-  const idx = document.getElementById("select-drink").selectedIndex;
-  const controller = document.getElementById("next-to-drink");
-  controller.innerHTML = "";
-
-  const d1 = document.createElement("div");
-  d1.className = "row";
-  d1.id = "drink1";
-  const d2 = document.createElement("div");
-  d2.className = "row";
-  d2.id = "drink2";
-
-  let img = document.createElement("img");
-
-  if (idx == 0) {
-    img.src = "/assets/drink/cola.png";
-    img.alt = "COLA";
-    img.className = "drink-1-img";
-    d1.appendChild(img);
-    if (isZero) {
-      img = document.createElement("img");
-      img.src = "/assets/drink/cola_zero.png";
-      img.alt = "COLA ZERO";
-      img.className = "drink-2-img";
-      d2.appendChild(img);
-    }
-    if (isSize) {
-      img = document.createElement("img");
-      img.src = "/assets/drink/cola_size.png";
-      img.alt = "COLA SIZE";
-      img.className = "drink-2-img";
-      d2.appendChild(img);
-    }
-  } else if (idx == 1) {
-    img.src = "/assets/drink/fanta.png";
-    img.alt = "FANTA";
-    img.className = "drink-1-img";
-    d1.appendChild(img);
-    if (isZero) {
-      img = document.createElement("img");
-      img.src = "/assets/drink/fanta_zero.png";
-      img.alt = "FANTA ZERO";
-      img.className = "drink-2-img";
-      d2.appendChild(img);
-    }
-    if (isSize) {
-      img = document.createElement("img");
-      img.src = "/assets/drink/fanta_size.png";
-      img.alt = "FANTA SIZE";
-      img.className = "drink-2-img";
-      d2.appendChild(img);
-    }
-  } else {
-    img.src = "/assets/drink/sprite.png";
-    img.alt = "SPRITE";
-    img.className = "drink-1-img";
-
-    d1.appendChild(img);
-    if (isZero) {
-      img = document.createElement("img");
-      img.src = "/assets/drink/sprite_zero.png";
-      img.alt = "SPRITE ZERO";
-      img.className = "drink-2-img";
-      d2.appendChild(img);
-    }
-    if (isSize) {
-      img = document.createElement("img");
-      img.src = "/assets/drink/sprite_size.png";
-      img.alt = "SPRITE SIZE";
-      img.className = "drink-2-img";
-      d2.appendChild(img);
-    }
-  }
-
-  controller.appendChild(d1);
-  controller.appendChild(d2);
-
-  document.getElementById("btn-reset").disabled = false;
-}
-
-function updateDrinkSizeFcn() {
+function checkDrinkSizeFcn() {
   const checked = document.getElementById("check-drink-size").checked;
   total_price += checked ? price_size_drink : -1 * price_size_drink;
   update_price();
   updateDrinkFcn();
 }
 
+function updateDrinkFcn() {
+  const isZero = document.getElementById("check-zero").checked;
+  const isSize = document.getElementById("check-drink-size").checked;
+  const idx = document.getElementById("select-drink").selectedIndex;
+
+  let drnk = "";
+
+  drnk += idx === 0 ? "c" : idx === 1 ? "f" : "s";
+  drnk += isZero ? "z" : "";
+  drnk += isSize ? "s" : "";
+
+  const img = document.createElement("img");
+  img.id = "drink-img";
+  img.className = "size-" + (isSize ? "plus" : "reg");
+
+  switch (drnk) {
+    case "c":
+      img.src = "/assets/drink/cola.png";
+      img.alt = "COLA";
+      break;
+    case "cz":
+      img.src = "/assets/drink/cola_zero.png";
+      img.alt = "COLA ZERO";
+      break;
+    case "cs":
+      img.src = "/assets/drink/cola_size.png";
+      img.alt = "COLA SIZE";
+      break;
+    case "czs":
+      img.src = "/assets/drink/cola_zero_size.png";
+      img.alt = "COLA ZERO SIZE";
+      break;
+    case "f":
+      img.src = "/assets/drink/fanta.png";
+      img.alt = "FANTA";
+      break;
+    case "fz":
+      img.src = "/assets/drink/fanta_zero.png";
+      img.alt = "FANTA ZERO";
+      break;
+    case "fs":
+      img.src = "/assets/drink/fanta_size.png";
+      img.alt = "FANTA SIZE";
+      break;
+    case "fzs":
+      img.src = "/assets/drink/fanta_zero_size.png";
+      img.alt = "FANTA ZERO SIZE";
+      break;
+    case "s":
+      img.src = "/assets/drink/sprite.png";
+      img.alt = "SPRITE";
+      break;
+    case "sz":
+      img.src = "/assets/drink/sprite_zero.png";
+      img.alt = "SPRITE ZERO";
+      break;
+    case "ss":
+      img.src = "/assets/drink/sprite_size.png";
+      img.alt = "SPRITE SIZE";
+      break;
+    case "szs":
+      img.src = "/assets/drink/sprite_zero_size.png";
+      img.alt = "SPRITE ZERO SIZE";
+      break;
+    default:
+  }
+
+  const controller = document.getElementById("next-to-drink");
+  controller.innerHTML = "";
+  controller.appendChild(img);
+
+  document.getElementById("btn-reset").disabled = false;
+}
+
+//////////////////////////////////////////////
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 
@@ -333,9 +331,9 @@ function add(i) {
       img.src = "/assets/meal/cheese.png";
       img.alt = "CHEESE";
       container = document.getElementById("meal-cheese");
-      if (cnt1 == max_addons) {
+      if (cnt1 === max_addons) {
         document.getElementById("btn-cheese-add").disabled = true;
-      } else if (cnt1 == 1) {
+      } else if (cnt1 === 1) {
         document.getElementById("btn-cheese-remove").disabled = false;
       }
       total_price += price1;
@@ -346,9 +344,9 @@ function add(i) {
       img.src = "/assets/meal/sauce.png";
       img.alt = "SAUCE";
       container = document.getElementById("meal-sauce");
-      if (cnt2 == max_addons) {
+      if (cnt2 === max_addons) {
         document.getElementById("btn-sauce-add").disabled = true;
-      } else if (cnt2 == 1) {
+      } else if (cnt2 === 1) {
         document.getElementById("btn-sauce-remove").disabled = false;
       }
       total_price += price2;
@@ -359,9 +357,9 @@ function add(i) {
       img.src = "/assets/meal/onion.png";
       img.alt = "ONION";
       container = document.getElementById("meal-onion");
-      if (cnt3 == max_addons) {
+      if (cnt3 === max_addons) {
         document.getElementById("btn-onion-add").disabled = true;
-      } else if (cnt3 == 1) {
+      } else if (cnt3 === 1) {
         document.getElementById("btn-onion-remove").disabled = false;
       }
       total_price += price3;
@@ -372,9 +370,9 @@ function add(i) {
       img.src = "/assets/meal/lettuce.png";
       img.alt = "LETTUCE";
       container = document.getElementById("meal-lettuce");
-      if (cnt4 == max_addons) {
+      if (cnt4 === max_addons) {
         document.getElementById("btn-lettuce-add").disabled = true;
-      } else if (cnt4 == 1) {
+      } else if (cnt4 === 1) {
         document.getElementById("btn-lettuce-remove").disabled = false;
       }
       total_price += price4;
@@ -385,9 +383,9 @@ function add(i) {
       img.src = "/assets/meal/tomato.png";
       img.alt = "TOMATO";
       container = document.getElementById("meal-tomato");
-      if (cnt5 == max_addons) {
+      if (cnt5 === max_addons) {
         document.getElementById("btn-tomato-add").disabled = true;
-      } else if (cnt5 == 1) {
+      } else if (cnt5 === 1) {
         document.getElementById("btn-tomato-remove").disabled = false;
       }
       total_price += price5;
@@ -415,9 +413,9 @@ function rem(i) {
       container.removeChild(child);
       cnt1--;
       total_price -= price1;
-      if (cnt1 == max_addons - 1) {
+      if (cnt1 === max_addons - 1) {
         document.getElementById("btn-cheese-add").disabled = false;
-      } else if (cnt1 == 0) {
+      } else if (cnt1 === 0) {
         document.getElementById("btn-cheese-remove").disabled = true;
       }
       break;
@@ -428,9 +426,9 @@ function rem(i) {
       container.removeChild(child);
       cnt2--;
       total_price -= price2;
-      if (cnt2 == max_addons - 1) {
+      if (cnt2 === max_addons - 1) {
         document.getElementById("btn-sauce-add").disabled = false;
-      } else if (cnt2 == 0) {
+      } else if (cnt2 === 0) {
         document.getElementById("btn-sauce-remove").disabled = true;
       }
       break;
@@ -441,9 +439,9 @@ function rem(i) {
       container.removeChild(child);
       cnt3--;
       total_price -= price3;
-      if (cnt3 == max_addons - 1) {
+      if (cnt3 === max_addons - 1) {
         document.getElementById("btn-onion-add").disabled = false;
-      } else if (cnt3 == 0) {
+      } else if (cnt3 === 0) {
         document.getElementById("btn-onion-remove").disabled = true;
       }
       break;
@@ -454,9 +452,9 @@ function rem(i) {
       container.removeChild(child);
       cnt4--;
       total_price -= price4;
-      if (cnt4 == max_addons - 1) {
+      if (cnt4 === max_addons - 1) {
         document.getElementById("btn-lettuce-add").disabled = false;
-      } else if (cnt4 == 0) {
+      } else if (cnt4 === 0) {
         document.getElementById("btn-lettuce-remove").disabled = true;
       }
       break;
@@ -467,9 +465,9 @@ function rem(i) {
       container.removeChild(child);
       cnt5--;
       total_price -= price5;
-      if (cnt5 == max_addons - 1) {
+      if (cnt5 === max_addons - 1) {
         document.getElementById("btn-tomato-add").disabled = false;
-      } else if (cnt5 == 0) {
+      } else if (cnt5 === 0) {
         document.getElementById("btn-tomato-remove").disabled = true;
       }
       break;
@@ -528,24 +526,30 @@ function colorConfigFcn() {
     : 3;
   const cls = "col bg" + idx + (idx === 3 ? "a" : "");
   document.getElementById("config").className = cls;
+  
+  document.getElementById("btn-reset").disabled = false;
 }
 
 function colorMealFcn() {
   const idx = document.getElementById("clr-meal-1").checked
-  ? 1
-  : document.getElementById("clr-meal-2").checked
-  ? 2
-  : 3;
-document.getElementById("meal").className = "col bg" + idx;
+    ? 1
+    : document.getElementById("clr-meal-2").checked
+    ? 2
+    : 3;
+  document.getElementById("meal").className = "col bg" + idx;
+
+  document.getElementById("btn-reset").disabled = false;
 }
 
 function colorNextToFcn() {
   const idx = document.getElementById("clr-next-to-1").checked
-  ? 1
-  : document.getElementById("clr-next-to-2").checked
-  ? 2
-  : 3;
-document.getElementById("next-to").className = "col bg" + idx;
+    ? 1
+    : document.getElementById("clr-next-to-2").checked
+    ? 2
+    : 3;
+  document.getElementById("next-to").className = "row bg" + idx;
+
+  document.getElementById("btn-reset").disabled = false;
 }
 
 //////////////////////////////////////////////
